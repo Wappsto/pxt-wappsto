5dd6b4c67933262c8256be250163d2a3411f672d
Andreas Bomholtz <andreas@seluxit.com>
2021-01-25 08:48:29 +0100

    Moved json back to main

--- main.ts
+++ main.ts
@@ -62,6 +62,32 @@ namespace Wappsto {
     let wappsto_connected: boolean = false;
     let queueFull: boolean = false;
 
+    function parseJSON(data: string): {[index: string]: string} {
+        let res: {[index: string]: string} = {};
+        if(data.indexOf("{") == 0 && data.indexOf("}") != -1) {
+            data = data.replace("{","").replace("}","").replaceAll("\"","")
+            let aData = data.split(",");
+            for(let i=0; i < aData.length; i++) {
+                let arr: Array<string> = aData[i].split(":");
+                res[arr[0]] = arr[1];
+            }
+        }
+
+        return res;
+    }
+
+    function generateJSON(data: {[index: string]: string}): string {
+        let json: string = "";
+        let keys = Object.keys(data);
+        for(let i = 0; i < keys.length; i++) {
+            if(json != "") {
+                json += ",";
+            }
+            json += '"'+keys[i]+'":"'+data[keys[i]]+'"';
+        }
+        return '{'+json+'}';
+    }
+
     function writeCommand(cmd: string): void {
         let json = {"command": cmd};
         writeToWappstobit(json);
